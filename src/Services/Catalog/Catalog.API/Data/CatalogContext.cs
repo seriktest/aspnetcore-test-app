8e922b703f82acca900369539993ad3123197d04
serik <podelu000@gmail.com>
2023-11-21 20:24:53 +0300

    add Entities

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,17 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data;
+
+public class CatalogContext : ICatalogContext
+{
+    public IMongoCollection<Product> Products { get; }
+
+    public CatalogContext(IConfiguration conf)
+    {
+        var client = new MongoClient(conf.GetValue<string>("DatabaseSettings:ConnectionString"));
+        var database = client.GetDatabase(conf.GetValue<string>("DatabaseSettings:DatabaseName"));
+        Products = database.GetCollection<Product>(conf.GetValue<string>("DatabaseSettings:CollectionName"));
+        CatalogContextSeed.SeedData(Products);
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContextSeed.cs
@@ -0,0 +1,44 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data;
+
+public static class CatalogContextSeed
+{
+    public static void SeedData(IMongoCollection<Product> products)
+    {
+        var existProduct = products.Find(p => true).Any();
+        if (!existProduct)
+        {
+            products.InsertManyAsync(GetPreconfiguredProducts());
+        }
+
+    }
+
+    private static IEnumerable<Product> GetPreconfiguredProducts()
+    {
+        return new List<Product>()
+        {
+            new Product()
+            {
+                Id = "602d2149e773f2a3990b47f5",
+                Name = "IPhone X",
+                Summary = "This is the summary of IPhone X",
+                Description = "This is the description of IPhone X",
+                ImageFile = "product-1.png",
+                Price = 950.00M,
+                Category = "Smart Phone"
+            },
+            new Product()
+            {
+                Id = "602d2149e773f2a3990b47f6",
+                Name = "Samsung 10",
+                Summary = "This is the summary of Samsung 10",
+                Description = "This is the description of Samsung 10",
+                ImageFile = "product-2.png",
+                Price = 840.00M,
+                Category = "Smart Phone"
+            }
+        };
+    }
+}
