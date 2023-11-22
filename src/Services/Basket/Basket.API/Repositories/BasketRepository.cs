ca2d084cf5886e275d525392c606ff5aedc69f9d
serik <podelu000@gmail.com>
2023-11-22 12:22:59 +0300

    repo to basket

---
+++ src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,34 @@
+using System.Text.Json;
+using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace Basket.API.Repositories; 
+
+public class BasketRepository : IBasketRepository
+{
+    private readonly IDistributedCache _cache;
+    
+    public BasketRepository(IDistributedCache cache)
+    {
+        _cache = cache ?? throw new ArgumentNullException(nameof(cache));
+    }
+
+
+    public async Task<ShoppingCart?> GetBasket(string userName) {
+        var basket = await _cache.GetStringAsync(userName);
+        if (string.IsNullOrEmpty(basket)) {
+            return null;
+        }
+        return JsonSerializer.Deserialize<ShoppingCart>(basket);
+    }
+
+    public async Task<ShoppingCart> UpdateBasket(ShoppingCart basket) {
+        await _cache.SetStringAsync(basket.UserName,
+            JsonSerializer.Serialize(basket));
+        return await GetBasket(basket.UserName);
+    }
+
+    public async Task DeleteBasket(string userName) {
+        await _cache.RemoveAsync(userName);
+    }
+}
