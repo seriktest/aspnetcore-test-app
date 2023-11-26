4ff2c10b2772ae2ff90f0ef713f1e4d176e0e7b1
serik <podelu000@gmail.com>
2023-11-26 09:01:03 +0300

    add Grpc server service to Discount.API

---
+++ src/Services/Discount/Discount.Grpc/Mapper/DiscountProfile.cs
@@ -0,0 +1,11 @@
+using AutoMapper;
+using Discount.Grpc.Entities;
+using Discount.Grpc.Services;
+
+namespace Discount.Grpc.Mapper; 
+
+public class DiscountProfile : Profile{
+    public DiscountProfile() {
+        CreateMap<Coupon, CouponModel>().ReverseMap();
+    }
+}
--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,3 +1,6 @@
+using Discount.API.Repositories;
+using Discount.Grpc.Extensions;
+using Discount.Grpc.Repositories;
 using Discount.Grpc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,12 +9,15 @@ var builder = WebApplication.CreateBuilder(args);
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 
 // Add services to the container.
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 builder.Services.AddGrpc();
+builder.Services.AddAutoMapper(typeof(Program));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+app.MigrateDatabase<Program>();
+app.MapGrpcService<DiscountService>();
 app.MapGet("/",
     () =>
         "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
