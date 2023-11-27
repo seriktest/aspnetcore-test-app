2456e1ef56d87c3b9a128651965640b747a376dc
serik <podelu000@gmail.com>
2023-11-27 23:02:38 +0300

    Maping models

--- src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
+++ src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
@@ -1,9 +1,13 @@
 ﻿using AutoMapper;
+using Ordering.Application.Features.Orders.Queries.GetOrdersList;
 
 namespace Ordering.Application.Mappings;
 
 public class MappingProfile : Profile
 {
+    public MappingProfile()
+    {
+        CreateMap<Domain.Entities.Order, OrdersVm>().ReverseMap();
+    }
 
-    
 }
