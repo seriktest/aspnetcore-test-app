69aff84ec47c64e213321102b364495b01fe977c
serik <podelu000@gmail.com>
2023-11-28 15:37:26 +0300

    init

--- src/Services/Ordering/Ordering.Application/Contracts/Persistence/IAsyncRepository.cs
+++ src/Services/Ordering/Ordering.Application/Contracts/Persistence/IAsyncRepository.cs
@@ -8,7 +8,7 @@ public interface IAsyncRepository<T> where T : EntityBase
     Task<IReadOnlyList<T>> GetAllAsync();
     Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate);
 
-    Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null,
+    Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>>? predicate = null,
                                     Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
                                     string? includeString = null,
                                     bool disableTracking = true);
