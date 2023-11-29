5d1cca540dcffc5b12e4548a7bb344a5be43b94f
serik <podelu000@gmail.com>
2023-11-29 21:14:36 +0300

    update ignored files

---
+++ src/GuessGame/GuessGame/Game.cs
@@ -0,0 +1,16 @@
+﻿namespace GuessGame;
+
+public abstract class Game
+{
+    private readonly IView _view;
+
+    protected Game(IView view)
+    {
+        _view = view;
+    }
+
+    protected virtual void Play()
+    {
+        _view.Show("\t\tStarting the game...\n\n");
+    }
+}
