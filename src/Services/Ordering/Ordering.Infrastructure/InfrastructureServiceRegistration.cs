3df49e4ab95440cdbb13899cc6581087ff16f6e8
serik <podelu000@gmail.com>
2023-12-10 17:31:53 +0300

    conn for email

--- src/Services/Ordering/Ordering.Infrastructure/InfrastructureServiceRegistration.cs
+++ src/Services/Ordering/Ordering.Infrastructure/InfrastructureServiceRegistration.cs
@@ -1,7 +1,10 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Contracts.Infrastructure;
 using Ordering.Application.Contracts.Persistence;
+using Ordering.Application.Models;
+using Ordering.Infrastructure.Mail;
 using Ordering.Infrastructure.Persistence;
 using Ordering.Infrastructure.Repositories;
 
@@ -15,6 +18,9 @@ public static class InfrastructureServiceRegistration {
         
         services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
         services.AddScoped<IOrderRepository, OrderRepository>();
+        
+        services.Configure<EmailSettings>(c => configuration.GetSection("EmailSettings"));
+        services.AddTransient<IEmailService, EmailService>();
         return services;
     }
 }
